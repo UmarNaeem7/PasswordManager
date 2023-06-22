03f64252fc1a2491b6aa5b056365fda3e720d42f
UmarNaeem7 <m.umarnaeem7@gmail.com>
2023-06-22 21:37:38 +0200

    New platform modifications

--- Password Manager/Properties/Resources.Designer.cs
+++ Password Manager/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Password_Manager.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -81,7 +81,7 @@ namespace Password_Manager.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Data Source=DESKTOP-C7RHKE6;Initial Catalog=PasswordManager;Integrated Security=True.
+        ///   Looks up a localized string similar to Server=localhost;Database=PasswordManager;Trusted_Connection=True;.
         /// </summary>
         internal static string connectionString {
             get {
